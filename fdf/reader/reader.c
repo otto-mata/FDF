6ddb2c6e000120976553c5a1ce770389e6737bdc
otto-mata <ottomata@proton.me>
2024-12-27 19:02:22 +0100

    color testing

--- fdf/reader/reader.c
+++ fdf/reader/reader.c
@@ -6,7 +6,7 @@
 /*   By: tblochet <tblochet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 04:00:30 by tblochet          #+#    #+#             */
-/*   Updated: 2024/12/23 19:10:29 by tblochet         ###   ########.fr       */
+/*   Updated: 2024/12/27 01:35:10 by tblochet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,6 +110,45 @@ static t_map_dim	*get_map_dimensions(char **lines)
 		dim->width++;
 	free_char2d(parts);
 	return (dim);
+}
+
+static int	c_lerp(int color1, int color2, double t)
+{
+	int	r;
+	int	g;
+	int	b;
+
+	r = (int)(((color1 >> 16) & 0xFF) + t * (((color2 >> 16) & 0xFF)
+				- ((color1 >> 16) & 0xFF)));
+	g = (int)(((color1 >> 8) & 0xFF) + t * (((color2 >> 8) & 0xFF)
+				- ((color1 >> 8) & 0xFF)));
+	b = (int)((color1 & 0xFF) + t * ((color2 & 0xFF) - (color1 & 0xFF)));
+	return ((r << 16) | (g << 8) | b);
+}
+
+static double	normalize(t_map_data *map, double z)
+{
+	return ((z - map->lowest) / (map->highest - map->lowest));
+}
+
+void	set_colors(t_map_data *map)
+{
+	int	color;
+
+	map->iter = map->nodes;
+	while (map->iter)
+	{
+		if (!map->iter->color)
+		{
+			color = c_lerp(0xff0000, 0x0000ff, normalize(map,
+						map->iter->coords.z));
+			if (color)
+				map->iter->color = color;
+			else
+				map->iter->color = 0xffffff;
+		}
+		map->iter = map->iter->next;
+	}
 }
 
 bool	populate_node(t_grid_node *node, char *raw)
